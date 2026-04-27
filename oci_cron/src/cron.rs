9d0bc9de4d0272722ca1f42530f888c1e23d2b82
LLSWE <tiilas777@gmail.com>
2026-04-27 23:56:57 +0000

    fix for prod

--- oci_cron/src/cron.rs
+++ oci_cron/src/cron.rs
@@ -2,6 +2,7 @@ use crate::mail;
 use crate::{model::AppConfig, script::get_vm};
 use std::sync::Arc;
 use tokio_cron_scheduler::{Job, JobScheduler, JobSchedulerError};
+use std::process; 
 
 pub async fn cron_fn(config: Arc<AppConfig>) -> Result<(), JobSchedulerError> {
     let sched = JobScheduler::new().await?;
@@ -17,6 +18,7 @@ pub async fn cron_fn(config: Arc<AppConfig>) -> Result<(), JobSchedulerError> {
                     Ok(status) => {
                         if status.success() {
                             println!("[SUCESSO] VM FOI CRIADA!!!");
+                            std::process::exit[0];
                             if let Err(e) = mail::send_confirm(&c.resend).await {
                                 eprintln!("[ERRO] Falha ao enviar o email: {:?}", e);
                             };
